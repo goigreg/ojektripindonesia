4ababb2791abe17cfab53c0e973f802df7ac2542
goigreg <goigregori@gmail.com>
2024-06-26 23:53:30 +0800

    Update Jstream

--- resources/views/admin/pages/login.blade.php
+++ resources/views/admin/pages/login.blade.php
@@ -14,6 +14,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <title>Ojek Trip Indonesia I {{$title}}</title>
+    <link rel="icon" href="{{asset('storage/company/Logogram.jpg')}}">
 </head>
 <body>
     <main>
